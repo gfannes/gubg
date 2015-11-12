fc1e5436932cc0025b74bc5d42af30c7c650cbc9
Geert Fannes <geert.fannes@auro-technologies.com>
2015-11-12 09:02:14 +0100

    added protection against uncloned submods

--- rakefile.rb
+++ rakefile.rb
@@ -2,6 +2,7 @@ def each_submod(&block)
     submods = %w[build std tools ui].map{|n|"gubg.#{n}"}
     submods.each do |sm|
         Dir.chdir(sm) do
+            raise("There is no rakefile present here: run a \"git submodule update --init\" to initialize and clone it") unless File.exist?('rakefile.rb')
             puts(">>>>>>>>> #{sm}")
             yield(sm)
             puts("<<<<<<<<< #{sm}\n\n")
