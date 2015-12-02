8830fc02720e46d336a754574e86e0b2afa1f014
Geert Fannes <geert.fannes@auro-technologies.com>
2015-12-02 14:13:06 +0100

    added gubg.tools.pm

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,5 @@
 def each_submod(&block)
-    submods = %w[build std tools ui].map{|n|"gubg.#{n}"}
+    submods = %w[build std tools tools.pm ui].map{|n|"gubg.#{n}"}
     submods.each do |sm|
         Dir.chdir(sm) do
             raise("There is no rakefile present here: run a \"git submodule update --init\" to initialize and clone it") unless File.exist?('rakefile.rb')
