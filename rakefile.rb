b43e911343351e7f60a738cb30df6afce18af079
Geert Fannes <geert.fannes@auro-technologies.com>
2015-12-04 15:26:50 +0100

    wip

--- rakefile.rb
+++ rakefile.rb
@@ -14,14 +14,7 @@ end
 
 def each_submod(&block)
     submods = %w[build std io tools tools.pm ui].map{|n|"gubg.#{n}"}
-    submods.each do |sm|
-        Dir.chdir(sm) do
-            raise("There is no rakefile present here: run a \"git submodule update --init\" to initialize and clone it") unless File.exist?('rakefile.rb')
-            puts(">>>>>>>>> #{sm}")
-            yield(sm)
-            puts("<<<<<<<<< #{sm}\n\n")
-        end
-    end
+    GUBG::each_submod(submods, &block)
 end
 
 task :declare do
