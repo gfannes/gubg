9468ab357851d2a105c60bb431f0e742a98817b6
Geert Fannes <geert.fannes@gmail.com>
2016-08-24 01:38:29 +0200

    added basic but working arduino uno support

--- arduino.rb
+++ arduino.rb
@@ -2,10 +2,15 @@ require(File.join(ENV['gubg'], 'shared'))
 require('gubg/build/Executable')
 include GUBG
 
-blink = Build::Executable.new('blink', arch: :uno)
-blink.add_sources('blink.cpp')
+task :clean do
+    rm_rf '.cache'
+end
 
-task :default do
-    sh "rake declare"
+task :default => :clean do
+    sh "rake define"
+    blink = Build::Executable.new('blink', arch: :uno)
+    blink.add_sources('blink.cpp')
+    blink.add_library_path(shared_dir('lib'))
+    # blink.add_library('arduino-core')
     blink.build
 end
